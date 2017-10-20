6e8852b9652afde09b550140bb69e6eac8941bd2
tafernandezc <30730989+tafernandezc@users.noreply.github.com>
2017-10-19 22:39:02 -0300

    Taladro

---
+++ Perforar.m
@@ -0,0 +1,16 @@
+function [distancias_a_fisis] = Perforar(input)
+%a1, a2, d, hueso, fisis
+hoyo = [posiciones(x,y,z) de la perforacion];
+distancias_a_fisis = [];
+
+for i = 1:size(hoyo)
+	%1 distancias_a_fisis(i).arriba =	Distancia a primer punto de fisis arriba
+    distancias_a_fisis(i).arriba =
+	%2 distancias_a_fisis(i).abajo = 	Distancia a primer punto de fisis abajo
+    distancias_a_fisis(i).abajo = 
+	%3 distancias_a_fisis(i).izquierda=	Distancia a primer punto de fisis izquierda
+    distancias_a_fisis(i).izquierda=	
+	%4 distancias_a_fisis(i).derecha =	Distancia a primer punto de fisis derecha
+    distancias_a_fisis(i).derecha =
+
+end
