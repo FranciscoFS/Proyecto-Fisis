94df20a25fa4f10248ca86430355116ae925b95a
tafernandezc <30730989+tafernandezc@users.noreply.github.com>
2017-11-21 21:10:35 -0300

    Avance

--- Cilindro.m
+++ Cilindro.m
@@ -23,13 +23,13 @@ btn1 = uicontrol('Style', 'pushbutton', 'String', 'Esta',...
         'Position', [20 20 50 20],...
         'Callback', 'elegida = 1');
 while (i > 0 && elegida == 0)
-imshow(V(:,:,i),[]) 
+imshow(V(:,:,i)) 
 k = waitforbuttonpress;
 
 if elegida == 1
     uiwait(msgbox('Ponga UN punto en el borde del hueso donde desea hacer la perforacion.'));
     [x,y]= getpts();
-    coordenada = [x,y,i];
+    coordenada = [y,x,i];
     close all
 elseif (k == 0 && elegida == 0)
     i = i+1;
@@ -45,14 +45,11 @@ end
 
 end
 
+coordenada(1) = Aproximar(coordenada(1));
+coordenada(2) = Aproximar(coordenada(2));
+
 %%
 %2. Elegir una dirección y distancia
-
-% [9:16, 15/11/2017] Sebastian Irarrazaval: Hola Tomás: hay que probar diámetros 5, 6 y 7 mm. Si solo se pudiera elegir uno: 6
-% [9:18, 15/11/2017] Sebastian Irarrazaval: Profundidad: entre 20 y 30 mm. Si hay que elegir uno: 30. Sería ideal evaluar también el brocado completo hasta la otra cortical
-% [9:18, 15/11/2017] Sebastian Irarrazaval: Perdona la variabilidad, pero hay varias técnicas quirúrgicas
-
-
 
 slice = V_seg.info{2,1};
 pixel = V_seg.info{1,1};
@@ -62,7 +59,7 @@ while bien == 0
 prompt = {'Ingrese angulo con respecto a z:','Ingrese angulo con respecto a x:', 'Ingrese profundidad en mm:', 'Ingrese el diametro de la perforacion'};
 dlg_title = 'Input';
 num_lines = 1;
-defaultans = {'0','0','30','6'};
+defaultans = {'0','0','30','2'};
 answer1 = inputdlg(prompt,dlg_title,num_lines,defaultans);
 answer1 = str2double(answer1);
 if (answer1(3)/slice) > size(V_seg.vol.orig,3)
@@ -75,10 +72,6 @@ else
     bien = 1;
 end    
 end
-
-coordenada(1) = Aproximar(coordenada(1));
-coordenada(2) = Aproximar(coordenada(2));
-coordenada(3) = Aproximar(coordenada(3));
 
 %%
 %3. Elegir el % de datos a los que se le quiere "no achuntar"
