1cd075bc257dad5d54fd698cd9be7c85432faa81
FranciscoFS <fdfernandez@uc.cl>
2018-06-25 23:57:24 -0400

    Correguir funciones
    Ahora si quedó bien

--- Cilindro_fx_final.m
+++ Cilindro_fx_final.m
@@ -1,17 +1,5 @@
 function porc = Cilindro_fx_final(V_seg,alpha,beta)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 %Direccion y distancia
-=======
-    %Direccion y distancia
->>>>>>> parent of 3391187... Update
-=======
-    %Direccion y distancia
->>>>>>> parent of 3391187... Update
-=======
-    %Direccion y distancia
->>>>>>> parent of 3391187... Update
 
     coordenada = V_seg.info{9};
     
@@ -19,9 +7,6 @@ function porc = Cilindro_fx_final(V_seg,alpha,beta)
     
    % fisis_usar = V_seg.femur.fisis;
     fisis_usar = V_seg.mascara == 2;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
    % hueso_usar = V_seg.mascara == 1;
     
     dz = V_seg.info{2,1};
@@ -46,41 +31,6 @@ function porc = Cilindro_fx_final(V_seg,alpha,beta)
     pixeles_z = z/dz;
     
     
-=======
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-    %hueso_usar = V_seg.femur.bones;
-
-    dz = V_seg.info{2,1};
-    dx = V_seg.info{1,1};
-
-    a1 = alpha;
-    a2 = beta;
-    mm = 30;%Profundidad
-    diametro = 2;
-
-    %profundidad
-    prof = cosd((a2))*mm;
-    pixeles_z = prof/dz;
-
-    %elevacion
-    elevacion = sind((a2))*mm;
-    pixeles_y = elevacion/dx;
-
-    %traslacion
-    tras = tand((a1))*prof;
-    pixeles_x = tras/dx;
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
     P1 = coordenada;
     P2 = [P1(1)+pixeles_x, P1(2) + pixeles_y, P1(3) + pixeles_z];
     P2 = Aproximar(P2);
@@ -91,46 +41,9 @@ function porc = Cilindro_fx_final(V_seg,alpha,beta)
     radio = diametro/2;
     radio_pix = Aproximar(radio/dx);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     pixeles_ya_sumados = zeros(size(fisis_usar));
 
     for i = 1:size(Z,2)
-=======
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-    contador = 0;
-    total_de_1s = sum(fisis_usar(:) == 1);
-
-
-    % f = figure;
-    % hold on
-    % fu= smooth3(fisis_usar);
-    % hu = smooth3(hueso_usar);
-    % p1= patch(isosurface(fu),'FaceColor','red','EdgeColor','none');
-    % p2= patch(isosurface(hu),'FaceColor','none','EdgeColor','blue','LineWidth',0.1,'EdgeAlpha','0.4');
-    % reducepatch(p2,0.01)
-    
-    pixeles_ya_sumados = zeros(size(fisis_usar));
-
-    % ax = gca;
-    % c = ax.DataAspectRatio;
-    % ax.DataAspectRatio= [dz,dz,dx];
-
-
-    for i = 1:size(Z,2)
-        im = fisis_usar(:,:,Z(i));
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
         x = Aproximar(X(i));
         y = Aproximar(Y(i));
 
@@ -138,42 +51,15 @@ function porc = Cilindro_fx_final(V_seg,alpha,beta)
         q = y - radio_pix : y + radio_pix;
 
         for j = 1:size(p,2)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         for t = 1:size(q,2)
         if (x-p(j))^2 + (y-q(t))^2 <= radio_pix^2
             if (pixeles_ya_sumados(p(j),q(t),Z(i)) == 0)
                 pixeles_ya_sumados(p(j),q(t),Z(i)) = 1;
             end
-=======
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-        for t = 1:size(p,2)
-        if (x-p(j)).^2 + (y-q(t)).^2 <= radio_pix.^2
-            if (pixeles_ya_sumados(p(j),q(t),Z(i)) == 0)
-                pixeles_ya_sumados(p(j),q(t),Z(i)) = 1;
-                if (im(p(j),q(t)) > 0)
-                    contador = contador + 1;
-                end 
-            end
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
         end 
         end
         end
     end
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     
 %     f = figure;
 %     hold on
@@ -198,20 +84,4 @@ function porc = Cilindro_fx_final(V_seg,alpha,beta)
     total_1s_resta = sum(delta(:));
     porc = ((total_de_1s - total_1s_resta)/total_de_1s)*100;
     
-=======
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-
-    porc = (contador/total_de_1s)*100;
-    %uiwait(msgbox({'Se ha perforado un ' num2str(porc) '% de la fisis'}));
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
-=======
->>>>>>> parent of 3391187... Update
 end
