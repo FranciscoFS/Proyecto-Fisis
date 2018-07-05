9950ac255a67b4e2802f8fc12c6d73f0b470e59e
tafernandezc <tafernandez@uc.cl>
2018-07-05 13:13:16 -0400

    Angulo_X

--- Rotar_3D.m
+++ Rotar_3D.m
@@ -15,8 +15,7 @@ function V_out = Rotar_3D(V_seg)
     [~,~,v1] = ind2sub(size(V_out.femur.bones),find(V_out.femur.bones > 0));
     Mid = round((max(v1)+min(v1))/2);
    
-    % Mi forma para endezar en Y
-    %(NUESTRA)
+%Endezar en Y
     
     z_atras = zeros(1,tam);
 
@@ -42,8 +41,8 @@ function V_out = Rotar_3D(V_seg)
     
     %subplot(1,2,1); plot(z_atras,1:tam);title('Antes') 
     
-    m = ((y2 -y1))/((z2 -z1));
-    m2 = ((y2 -y1)*dx)/((z2 -z1)*dz);
+    m = ((y2 -y1))/((z2 -z1)); %angulo a girar en datos para que quede alineado
+    m2 = ((y2 -y1)*dx)/((z2 -z1)*dz); %angulo en mundo real
     Theta_Y = atand(m);
     Theta_Y_2 = atand(m2);
     V_out.info{7,1} = Theta_Y;
@@ -54,6 +53,51 @@ function V_out = Rotar_3D(V_seg)
     eje_Y = 'Y';
     %eje_Y = [0 1 0];
     V_out = Rotar(V_out,Theta_Y,eje_Y);
+
+%Girar en x    
+
+        z_abajo = zeros(1,tam);
+
+    for i=1:tam
+
+        im = V_seg.femur.bones(:,:,i);
+        [row, ~] = find(im);
+        toprow1 = max(row);
+
+        if isempty(toprow1) == 0
+            
+            z_abajo(i) = toprow1;
+            
+        end
+
+    end
+
+    z1 = find(z_abajo(1:Mid) == max(z_abajo(1:Mid)),1,'last');
+    z2 = find(z_abajo(Mid+1:end) == max(z_abajo(Mid+1:end)),1,'first') + Mid;
+
+    y1 = z_abajo(z1);
+    y2 = z_abajo(z2);
+    
+    %subplot(1,2,1); plot(z_atras,1:tam);title('Antes') 
+
+    m = ((y2 -y1))/((z2 -z1)); %angulo a girar en datos para que quede alineado
+    m2 = ((y2 -y1)*dx)/((z2 -z1)*dz); %angulo en mundo real
+    Theta_X = atand(m);
+    Theta_X_2 = atand(m2);
+    V_out.info{9,1} = Theta_X;
+    V_out.info{9,2} = Theta_X_2;
+    
+    % Respecto al eje largo del femur
+    
+    eje_X = 'X';
+    %eje_Y = [0 1 0];
+    V_out = Rotar(V_out,Theta_X,eje_X);
+
+
+
+
+
+
 %     
 %     tam_after = size(V_out.femur.bones,3);
 %     z_atras_after = zeros(1,tam_after);
