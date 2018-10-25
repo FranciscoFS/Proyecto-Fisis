3ae962e1c03cb2745271114e94bd21f87968e401
tafernandezc <tafernandez@uc.cl>
2018-10-25 18:19:45 -0300

    Arreglada la distancia

--- Distancia_a_crotical_lateral.m
+++ Distancia_a_crotical_lateral.m
@@ -1,16 +1,15 @@
-function [mm_dist_cada_angulo] = Distancia_a_crotical_lateral(V_seg)
-    ang = 10;
-    
+function [mm_dist_cada_angulo] = Distancia_a_crotical_lateral(V_seg,ang)
+
     dz = V_seg.info{2,1};
     dx = V_seg.info{1,1};
     
-    mm = 300; %asegurarse que atraviese (podria ser cualquier cosa)
+    mm = 200; %asegurarse que atraviese (podria ser cualquier cosa)
     coordenada = V_seg.info{8};
     Femur = V_seg.mascara == 1;
     Fisis = V_seg.mascara == 2;
     Femur_total = (Femur + Fisis)>0;
     
-for i = 1:ang
+for i = 0:ang
 
     [z,x,y] = sph2cart(0,deg2rad(i),mm);
     %dif_x
@@ -20,6 +19,8 @@ for i = 1:ang
     %dif_z
     pixeles_z = z/dz;
     
+    
+    
     P1 = [coordenada(1),coordenada(2),coordenada(3)];
     P2 = [P1(1)+pixeles_x, P1(2) + pixeles_y, P1(3) + pixeles_z];
     P2 = Aproximar(P2);
@@ -27,12 +28,10 @@ for i = 1:ang
     [X, Y, Z] = bresenham_line3d(P1, P2);
     
     for y = size(Z,2):-1:1
-        if Z(y)<=size(Femur_total,3)
+        if Z(y)<= size(Femur_total,3)
             if Femur_total(X(y),Y(y),Z(y))>0
-
                 ultimo_punto = [X(y),Y(y),Z(y)];
-                pixeles_dist=pdist([P1;ultimo_punto],'euclidean');
-                mm_dist_cada_angulo(i) = pixeles_dist*dz;
+                mm_dist_cada_angulo(i+1) = sqrt(((ultimo_punto(3)-P1(3))*dz)^2 +((ultimo_punto(2)-P1(2))*dx)^2);
                 break
             end
         end
