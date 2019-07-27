7d45976db803a2fc2493e7588d23c87b1bd4c3fc
tafernandezc <tafernandez@uc.cl>
2019-07-27 17:39:00 -0400

    ultimo cambio

--- Para_TAC.m
+++ Para_TAC.m
@@ -1,6 +1,6 @@
-function V_seg = Para_TAC(V_seg)
+function V_seg = Para_TAC(V_seg,n)
 %SIC-TAC, TT-TG
-uiwait(msgbox(Base_datos(1).RM.info{8,1}));
+uiwait(msgbox(Base_datos(n).RM.info{8,1}));
 V_seg = Elegir_puntos_para_TAC(V_seg);
 V_seg = Calculos_TAC(V_seg);
 end
